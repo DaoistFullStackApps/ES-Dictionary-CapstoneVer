83ff25449a07914f0827a3c0cdccdf7252997b82
daoistcoder <deservekindness@gmail.com>
2023-06-03 08:24:29 +0800

    setup Routes -WIP
    update router.jsx
    adding properties on createBrowserRouter with each component inside views

--- ES-Dictionary-Laravel-React-App/React-ES-Dictionary/src/router.jsx
+++ ES-Dictionary-Laravel-React-App/React-ES-Dictionary/src/router.jsx
@@ -1,7 +1,27 @@
 import {createBrowserRouter} from "react-router-dom";
+import Login from "./views/Login.jsx";
+import Dictionary from "./views/Dictionary.jsx";
+import Hero from "./views/Hero.jsx";
+import Users from "./views/Users.jsx";
+
 
 const router = createBrowserRouter([
-
+	{
+		path: '/login',
+		element: <Login />	
+	},
+	{
+		path: '/dictionary',
+		element: <Dictionary />	
+	},
+	{
+		path: '/hero',
+		element: <Hero />	
+	},
+	{
+		path: '/users',
+		element: <Users />	
+	},
 ])
 
 export default router;
