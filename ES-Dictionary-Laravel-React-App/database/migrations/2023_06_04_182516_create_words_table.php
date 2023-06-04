917b6686b70a431636d6cb8da17df888e4dda5dc
daoistcoder <deservekindness@gmail.com>
2023-06-05 02:31:32 +0800

    setup CRUD

---
+++ ES-Dictionary-Laravel-React-App/database/migrations/2023_06_04_182516_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('word')->unique();
+            $table->text('definition');
+            $table->string('part_of_speech');
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+};
